731413030578863a02f3b6684631347609014e7a
Igor Stolbov <Stolbov.Ig.Ig@sberbank.ru>
2019-07-29 10:02:14 +0300

    missed file

---
+++ YetAnotherXamlPad/EditorState/WindowPosition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+
+namespace YetAnotherXamlPad.EditorState
+{
+    [Serializable]
+    internal struct WindowPosition
+    {
+        public Rect Location { get; set; }
+
+        public WindowState WindowState { get; set; }
+
+        public void ApplyTo(Window window)
+        {
+            window.Left = Location.Left;
+            window.Top = Location.Top;
+            window.Width = Location.Width;
+            window.Height = Location.Height;
+            window.WindowState = WindowState;
+        }
+    }
+}
